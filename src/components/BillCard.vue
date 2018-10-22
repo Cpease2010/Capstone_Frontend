04aa48d14f6ea5f6c78cc45f08d1b54a6283e68d
Cory Pease <coryrpease@gmail.com>
2018-10-22 13:57:23 -0600

    model myAccount page w/ route, pulls user data and saves to data

---
+++ src/components/BillCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-card bg-variant="success" text-variant="white" header="Success" class="text-center">
+      <p class="card-text">{{bill.companyName}}</p>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BillCard',
+    props: ['bill']
+  }
+</script>
+
+<style scoped>
+.card-deck .card {
+  flex: none;
+  width: 30%;
+  margin: 20px;
+}
+</style>
