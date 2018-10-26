36260c5b9e032b402fa4ac960aa00f8dc6f9d466
Cory Pease <coryrpease@gmail.com>
2018-10-26 10:31:43 -0600

    cleaning up repo

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -86,13 +86,8 @@ export default {
   },
   mounted() {
       this.getUserID() 
-<<<<<<< HEAD
-      .then(userID => 
-      this.$http.post(this.URLS.addAccountURL+userID,{})
-=======
       .then(user => 
       this.$http.post(this.URLS.addAccountURL+user.user_ID, user)
->>>>>>> master
       )
       .then(result => result.json())
       .then(account => this.accountInfo = account.newUser)
