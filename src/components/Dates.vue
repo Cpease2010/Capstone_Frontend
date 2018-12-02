dbf01c17547595dda87fcdc3b13bd9d90fb3bb72
Cory Pease <coryrpease@gmail.com>
2018-12-01 19:18:04 -0700

    working on getting month to change when payday does

--- src/components/Dates.vue
+++ src/components/Dates.vue
@@ -35,22 +35,51 @@
       setDate(date){
         (date.year % 4 === 0) ? months["2"].days = 29 : false;
         this.limit = months[date.month].days
-        this.startDate = 7
+        this.startDate = date.day
         this.year = date.year
         return date
       },
       calculatePayDates(date){
-        let month = date.month,
-            limits = [];
-        for (let i = 0; i < 12; i++) {
-          let limit = months[month].days,
-          carryover = limit + 14;
-          limits.push(carryover);
-          console.log(months[month].name,months[month].days,limit)
-          month++
-          month = (month > 12) ? 1 : month;
-        }
-        console.log(limits)
+        //check that payDay is based on user input and not date variable
+        let month = 12,
+            payDates = (payDay, limits, month, payDates = []) => {
+                  limit = limits[1];
+              for (let j = 0; j < limits.length; j++) {
+                
+              console.log(month);
+              
+              
+              month = (payDay > limit) ? month+=1 : month;
+              console.log('last',month)
+              payDay = (payDay > limit) ? payDay % limit : payDay;
+              // console.log('payDay After', payDay)
+              limit=limits[month]
+              month = (month >= 12) ? 1 : month;
+              // console.log(payDates)
+              payDates.push({'payDay': payDay, 'limit': limit, 'month': month})
+              payDay += 14
+              //add 14 for payDay
+              //payDay = ;
+              //limit = limits
+              //push startDate into payDates
+              // //
+              // console.log(limit)
+              }
+              return payDates
+            },
+            limits = (month, limitsArr = []) => {
+              for (let i = 0; i < 12; i++) {
+                let limit = months[month].days,
+                    carryover = limit + 14;
+                limitsArr.push(limit);
+                month++
+                month = (month > 12) ? 1 : month;
+              }
+              return limitsArr
+            };  
+            
+        payDates(this.startDate, limits(month), month)
+        // console.log(limits(month))
       }
     },
     computed: {
